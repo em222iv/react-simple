2a14b557758c85950dfbc474bc3ebc7aa2488aa8
dav <dg222cs@student.lnu.se>
2015-12-10 13:09:04 +0100

    It was not possible to compile ES2015 Imports with your old compiler, so i changed to babelify and updated your package.json, also some minor fix in code to make all valid javascript acording to babel

--- src/components/nav.js
+++ src/components/nav.js
@@ -6,9 +6,7 @@ var React = require('react'),
     auth = require('../auth');
 
 
-this.divStyle = {
-    left: -250
-};
+
 var Nav = React.createClass({
     propTypes: {
         login: ptypes.func.isRequired
@@ -42,6 +40,12 @@ var Nav = React.createClass({
         );
     }
 });
+
+
+Nav.divStyle = {
+    left: -250
+};
+
 var mapStateToProps = function(state){
     return state.login;
 };
